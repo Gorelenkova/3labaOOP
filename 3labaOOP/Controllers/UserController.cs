2d36b0d3a0fcddacdf603ab2c643b9984a1217ed
Gorelenkova <gorelenkova0404@mail.ru>
2024-04-22 23:13:17 +0300

    добавили контроллеры для user

--- 3labaOOP/Controllers/UserController.cs
+++ 3labaOOP/Controllers/UserController.cs
@@ -12,6 +12,24 @@ namespace _3labaOOP.Controllers
         {
             this._context = _context;
         }
+        [HttpGet("GetAll")]
+        public ActionResult<List<User>> GetUserAll()
+        {
+            var users = _context.Users.ToList();
+            return users;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<string> GetUserId(int id)
+        {
+            var user = _context.Users.Find(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
 
         [HttpPost("AddUser")]
         public ActionResult<string> CreateUser(string Name, string LastName)
@@ -27,6 +45,38 @@ namespace _3labaOOP.Controllers
             _context.SaveChanges();
 
             return "Sce horosho";
+
+        }
+
+        public class UpdatedUser
+        {
+            public string Name { get; set; }
+            public string LastName { get; set; }
+        }
+        [HttpPut("PutUser")]
+        public ActionResult<string> PutUser(int id, UpdatedUser updatedUser)
+        {
+            var user = _context.Users.Find(id);
+            if (user == null)
+            {
+                return NotFound(id);
+            }
+            user.Name = updatedUser.Name;
+            user.LastName = updatedUser.LastName;
+            _context.SaveChanges();
+            return Ok(user);
+        }
+        [HttpDelete("DeleteUser")]
+        public ActionResult<string> DeleteUser(int id)
+        {
+            var user = _context.Users.Find(id);
+            if (user == null)
+            {
+                return NotFound(id);
+            }
+            _context.Remove(user);
+            _context.SaveChanges();
+            return Ok(user);
 
         }
     }
