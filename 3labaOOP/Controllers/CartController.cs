279bb86e1e2ee15e814257273dc1540cf35d8fb2
Gorelenkova <gorelenkova0404@mail.ru>
2024-04-24 21:37:15 +0300

    готовый(почти) бэк

--- 3labaOOP/Controllers/CartController.cs
+++ 3labaOOP/Controllers/CartController.cs
@@ -1,5 +1,7 @@
-﻿using _3labaOOP.Models;
+﻿using _3labaOOP.dto.CartDtos;
+using _3labaOOP.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace _3labaOOP.Controllers
 {
@@ -15,8 +17,13 @@ namespace _3labaOOP.Controllers
         [HttpPost("AddInCart")]
         public ActionResult<string> AddInCart(int userid, int productid)
         {
-            var user = _context.Users.FirstOrDefault(x => x.Id == userid);
+
             var product = _context.Products.FirstOrDefault(x => x.Id == productid);
+            var user = _context.Users.Include(x => x.Cart).FirstOrDefault(x => x.Id == userid);
+            if (user == null || product == null)
+            {
+                return NotFound();
+            }
 
             var cartProduct = new CartProduct()
             {
@@ -26,6 +33,27 @@ namespace _3labaOOP.Controllers
             _context.CartProducts.Add(cartProduct);
             _context.SaveChanges();
             return Ok("Добавлено в корзину");
+        }
+        [HttpGet("GetOrderById")]
+        public ActionResult<List<CartProductDto>> GetUsersOrderByUserId(int UserId)
+        {
+            var user = _context.Users.Include(x => x.Cart).FirstOrDefault(x => x.Id == UserId);
+
+            var cartproduct = _context.CartProducts.Where(x => x.Cart == user.Cart).Select(x => x.ProductId).ToList();
+
+            var products = new List<Product>();
+
+            foreach (var p in cartproduct)
+            {
+                products.Add(_context.Products.FirstOrDefault(x => x.Id == p));
+            }
+
+
+            var productdto = products.Select(x => new CartProductDto { Name = x.Name, Description = x.Description, Price = x.Price }).ToList();
+
+            return productdto;
+
+
         }
 
     }
---
+++ 3labaOOP/DTOs/CartDtos/CartProductDto.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _3labaOOP.dto.CartDtos
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartProductDto : ControllerBase
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+        public string Description { get; set; }
+
+    }
+}
